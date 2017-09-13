bdfa86680ae4a144481fcad99fbed795b1412cda
Peter Ryznar <piedro@gmx.at>
2017-09-13 20:15:24 +0200

    BT65 Constructor & Begin Play Timing

--- BattleTank/Source/BattleTank/Private/Tank.cpp
+++ BattleTank/Source/BattleTank/Private/Tank.cpp
@@ -12,13 +12,18 @@ ATank::ATank()
 {
 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = false;
+
+	auto TankName = GetName();
+	UE_LOG(LogTemp, Warning, TEXT("%s PDRO: Constructor_C"), *TankName)
 }
 
 // Called when the game starts or when spawned
 void ATank::BeginPlay()
 {
-	Super::BeginPlay();
-	
+	Super::BeginPlay(); // Needed for Blueprint BeginPlay to run!
+
+	auto TankName = GetName();
+	UE_LOG(LogTemp, Warning, TEXT("%s PDRO: BeginPlay_C"), *TankName)
 }
 
 void ATank::AimAt(FVector HitLocation)
--- BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
@@ -35,7 +35,5 @@ void UTankMovementComponent::RequestDirectMove(const FVector& MoveVelocity, bool
 	
 	auto RightThrow = FVector::CrossProduct(TankForward, AIForwardIntention).Z;
 	IntendTurnRight(RightThrow);
-	
-	UE_LOG(LogTemp, Warning, TEXT("Right: %f Forward: %f"), RightThrow, ForwardThrow)
 }
 
